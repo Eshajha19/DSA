469308507cc1353fea220a715394289b17a58ecd
Esha Jha <149310101+Eshajha19@users.noreply.github.com>
2026-03-15 19:10:23 +0530

    Create 01Matrix.cpp

---
+++ Graph/MaxAreaOf/01Matrix.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        queue<pair<int,int>>q;
+        vector<vector<int>>visited(mat.size(),vector<int>(mat[0].size(),-1));
+        for(int i=0;i<mat.size();i++){
+            for(int j=0;j<mat[0].size();j++){
+                if(mat[i][j]==0){
+                    q.push({i,j});
+                    visited[i][j]=0;
+                }
+            }
+        }
+        while(!q.empty()){
+            auto a=q.front();
+            q.pop();
+            int x=a.first;
+            int y=a.second;
+            vector<vector<int>>dir={{1,0},{-1,0},{0,1},{0,-1}};
+            for(int k=0;k<4;k++){
+                int nx=x+dir[k][0];
+                int ny=y+dir[k][1];
+                if(nx>=0 && nx<mat.size() && ny>=0 && ny<mat[0].size() && visited[nx][ny]==-1){
+                    q.push({nx,ny});
+                    visited[nx][ny]=visited[x][y]+1;
+                }
+            }
+        }
+        return visited;
+    }
+};
